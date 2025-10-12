803a3bcf2f3506ef0605775b2579c75736087150
Zsolt Fekete <nxu@nxu.hu>
2025-10-12 10:05:48 +0200

    🐛 Only show users own parent categories in subcategory form

--- app/Filament/Resources/TransactionSubcategories/Schemas/TransactionSubcategoryForm.php
+++ app/Filament/Resources/TransactionSubcategories/Schemas/TransactionSubcategoryForm.php
@@ -2,10 +2,13 @@
 
 namespace App\Filament\Resources\TransactionSubcategories\Schemas;
 
+use App\Models\TransactionCategory;
+use Filament\Facades\Filament;
 use Filament\Forms\Components\ColorPicker;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
+use Illuminate\Database\Eloquent\Builder;
 
 class TransactionSubcategoryForm
 {
@@ -21,7 +24,12 @@ class TransactionSubcategoryForm
                     ->label('Szín'),
 
                 Select::make('transaction_category_id')
-                    ->relationship('parentCategory', 'title')
+                    ->relationship('parentCategory', 'title', modifyQueryUsing: function (Builder $builder) {
+                        return $builder
+                            ->setModel(new TransactionCategory) // ??? Pretty sure it's a Filament bug, it has to be specified
+                            ->where('user_id', Filament::auth()->id())
+                            ->orderBy('order_column');
+                    })
                     ->required(),
             ]);
     }
