7afc1b233fcda167e74c0614893e1cc676897142
Zsolt Fekete <nxu@nxu.hu>
2025-10-19 11:40:23 +0200

    ✨ Sort spending items on dashboard

--- app/Filament/Widgets/MonthlyTransactionsWidget.php
+++ app/Filament/Widgets/MonthlyTransactionsWidget.php
@@ -29,6 +29,7 @@ class MonthlyTransactionsWidget extends StatsOverviewWidget
             ->selectRaw('SUM(amount) as sum')
             ->selectRaw('transaction_category_id')
             ->groupBy('transaction_category_id')
+            ->orderByDesc('sum')
             ->get()
             ->map(function ($t) use ($categories) {
                 $sum = number_format($t->sum, thousands_separator: ' ').' Ft';
