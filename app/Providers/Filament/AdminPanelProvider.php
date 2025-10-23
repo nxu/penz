6979163766385c83b1510f0283bc90a58c5426fd
Patri <kovacs.patricia@fws.hu>
2025-10-23 13:08:25 +0200

    Change icons

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,7 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
-            ->favicon('/icon.png')
+            ->favicon('/admin-icon.svg')
             ->viteTheme('resources/css/filament/admin/theme.css')
             ->login()
             ->profile()
